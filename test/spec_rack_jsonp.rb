bf47274ce262d5dac47a88285ef36ed2bf7e3385
Cameron Walters <cameron.walters@gmail.com>
2009-01-23 00:56:33 -0800

    Set proper Content-Length header when wrapping response with JSONP callback.

--- test/spec_rack_jsonp.rb
+++ test/spec_rack_jsonp.rb
@@ -10,7 +10,16 @@ context "Rack::JSONP" do
     body.should == 'foo({"bar":"foo"})'
   end
   
-  specify "should not change anything if no :callback param is provided" do
+  specify "should modify the content length to the correct value when callback is provided" do
+    test_body = '{"bar":"foo"}'
+    callback = 'foo'
+    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, test_body] }
+    request = Rack::MockRequest.env_for("/", :input => "foo=bar&callback=#{callback}")
+    headers = Rack::JSONP.new(app).call(request)[1]
+    headers['Content-Length'].should == (test_body.length + callback.length + 2).to_s # 2 parentheses
+  end
+  
+  specify "should not change anything if no callback param is provided" do
     app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, '{"bar":"foo"}'] }
     request = Rack::MockRequest.env_for("/", :input => "foo=bar")
     body = Rack::JSONP.new(app).call(request).last
